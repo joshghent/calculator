3c10eb7335a6ecbb86b6b4af322b768034498eac
Josh Ghent <me@joshghent.com>
2015-12-20 21:13:41 +0000

    Rewrote init function...again :D

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ var output = 0;
 
 var display = document.getElementById('display');
 
-var init = {
-    assignButtons: function(id, number){
+var init = function(){
+    var assignButtons = function(id, number){
         document.getElementById(id).addEventListener('click', function(){
             currentNumber += number;
+            display.innerHTML += number + ' ';
         });
     }
+    assignButtons('zero', 0);
+    assignButtons('one', 1);
+    assignButtons('two', 2);
+    assignButtons('three', 3);
+    assignButtons('four', 4);
+    assignButtons('five', 5);
+    assignButtons('six', 6);
+    assignButtons('seven', 7);
+    assignButtons('eight', 8);
+    assignButtons('nine', 9);
+    
+    var sumButton = document.getElementById('sumButton');
+    sumButton.addEventListener('click', calculate);
 };
+
 
 var calculate = function(){
     
