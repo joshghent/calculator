95bb667d8d2c76b3ca6dafefde758749c0f70b67
Josh Ghent <me@joshghent.com>
2016-12-30 02:53:54 +0000

    Fixed 99% of eslint issues. Renamed methods. Removed fontawesome file.

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ function getKeyTypeLastPressed() {
 
   let lastKeyPressedType;
 
-  for (let i = 0; i < operators.length; i++) {
-    if (lastKeyInDisplayArray == operators[i]) {
+  for (let i = 0; i < operators.length; i += 1) {
+    if (lastKeyInDisplayArray === operators[i]) {
       lastKeyPressedType = 'operator';
     } else {
       lastKeyPressedType = 'number';
@@ -25,93 +25,88 @@ function getKeyTypeLastPressed() {
   return lastKeyPressedType;
 }
 
-function number_key_pressed(num) {
+function numberKeyPressed(num) {
   calculatorDisplayContents.push(num);
   display.value = calculatorDisplayContents.join('');
 }
 
 // Bind the number keys to the work as if the number keys on the browser are clicked
-keyboardJS.bind('1', (e) => { number_key_pressed(1); });
-keyboardJS.bind('2', (e) => { number_key_pressed(2); });
-keyboardJS.bind('3', (e) => { number_key_pressed(3); });
-keyboardJS.bind('4', (e) => { number_key_pressed(4); });
-keyboardJS.bind('5', (e) => { number_key_pressed(5); });
-keyboardJS.bind('6', (e) => { number_key_pressed(6); });
-keyboardJS.bind('7', (e) => { number_key_pressed(7); });
-keyboardJS.bind('8', (e) => { number_key_pressed(8); });
-keyboardJS.bind('9', (e) => { number_key_pressed(9); });
-keyboardJS.bind('0', (e) => { number_key_pressed(0); });
+keyboardJS.bind('1', (e) => { numberKeyPressed(1); });
+keyboardJS.bind('2', (e) => { numberKeyPressed(2); });
+keyboardJS.bind('3', (e) => { numberKeyPressed(3); });
+keyboardJS.bind('4', (e) => { numberKeyPressed(4); });
+keyboardJS.bind('5', (e) => { numberKeyPressed(5); });
+keyboardJS.bind('6', (e) => { numberKeyPressed(6); });
+keyboardJS.bind('7', (e) => { numberKeyPressed(7); });
+keyboardJS.bind('8', (e) => { numberKeyPressed(8); });
+keyboardJS.bind('9', (e) => { numberKeyPressed(9); });
+keyboardJS.bind('0', (e) => { numberKeyPressed(0); });
 
 // This function runs when an operator key, multiple, add etc. is pressed
 // The type is a parameter passed to identify the operation of the key that was pressed
-function operator_key_pressed(type) {
+function operatorKeyPress(type) {
     // Only allow the pressing on a number key if the last key pressed does not match an operator and the display is not blank
-  if (getKeyTypeLastPressed() != 'operator' && calculatorDisplayContents.join('') != '') {
-        // Divide type
-    if (type == '/') {
-        calculatorDisplayContents.push('÷');
-        display.value = calculatorDisplayContents.join('');
-      }
-        // Multiple type
-    else if (type == '*') {
-        calculatorDisplayContents.push('×');
-        display.value = calculatorDisplayContents.join('');
-      }
-        // Add type
-    else if (type == '+') {
-        calculatorDisplayContents.push('+');
-        display.value = calculatorDisplayContents.join('');
-      }
-        // Subtract type
-    else if (type == '-') {
-        calculatorDisplayContents.push('-');
-        display.value = calculatorDisplayContents.join('');
-      } else if (type == 'decimal') {
-          calculatorDisplayContents.push('.');
-          display.value = calculatorDisplayContents.join('');
-        }
-        // In the case that it did not find one of those four types (unlikely but always include error handling!)
-    else {
-          console.log('Error :(');
-        }
+  if (getKeyTypeLastPressed() !== 'operator' && calculatorDisplayContents.join('') !== '') {
+    // Divide type
+    if (type === '/') {
+      calculatorDisplayContents.push('÷');
+      display.value = calculatorDisplayContents.join('');
+    // Multiple type
+    } else if (type === '*') {
+      calculatorDisplayContents.push('×');
+      display.value = calculatorDisplayContents.join('');
+    // Add type
+    } else if (type === '+') {
+      calculatorDisplayContents.push('+');
+      display.value = calculatorDisplayContents.join('');
+    // Subtract type
+    } else if (type === '-') {
+      calculatorDisplayContents.push('-');
+      display.value = calculatorDisplayContents.join('');
+    } else if (type == 'decimal') {
+      calculatorDisplayContents.push('.');
+      display.value = calculatorDisplayContents.join('');
+    } else {
+      console.error('Sorry there was a problem processing your calculation');
+    }
   }
 }
 
 // This function runs when the equals button is pressed
-function evaluate_display_value() {
+function calculate() {
     // First let's make a seperate array from the display array so we can work with it without messing with the users display.
-  const computation_logic_array = calculatorDisplayContents;
+  const logicArray = calculatorDisplayContents;
 
-    // Next we join the calculatorDisplayContents as eval() requires a string
-    // I'm terrible at naming variables, don't hate :D
-  let string_to_do_calculation_on = computation_logic_array.join('');
+  // Next we join the calculatorDisplayContents as eval() requires a string
+  // I'm terrible at naming variables, don't hate :D
+  let userCalculation = logicArray.join('');
 
-    // Replace the multiple and divide symbols with the actual ones so we can run eval
-  string_to_do_calculation_on = string_to_do_calculation_on.replace(/×/g, '*').replace(/÷/g, '/');
+  // Replace the multiple and divide symbols with the actual ones so we can run eval
+  userCalculation = userCalculation.replace(/×/g, '*').replace(/÷/g, '/');
 
-    // Next we do eval and store the output as a variable
-  const output_calculation = eval(string_to_do_calculation_on);
+  // Next we do eval and store the output as a variable
+  const calculationOutput = eval(userCalculation);
 
-    // Output the calculation to the display
-  display.value = output_calculation;
+  // Output the calculation to the display
+  display.value = calculationOutput;
 
-    // Reset the display array to the sum (this should also prevent you from pressing number keys, I think)
-  calculatorDisplayContents = [output_calculation];
+  // Reset the display array to the sum (this should also prevent you from pressing number keys, I think)
+  calculatorDisplayContents = [calculationOutput];
 }
 
-function square_number() {
-    // First we want to get the output of the current display in case people want to do things like (5 + 3)^2
-  evaluate_display_value();
-
-    // Next we get the value from the display value and multiple it by itself and store it as the variable 'square_number_output'
-    // Since we just ran the evaluate_display_value function then there can only be 1 item in the display value array
+function squareNumber() {
+  // First we want to get the output of the current display in case people want to do things like (5 + 3)^2
+  calculate();
+  
+  // Next we get the value from the display value and multiple it by itself and store it as the variable 'square_number_output'
+  // Since we just ran the evaluateDisplayValue function then there can only be 1 item in the display value array
   const squareNumberOutput = calculatorDisplayContents[0] * calculatorDisplayContents[0];
 
-    // Then we reset the display value
+  // Then we reset the display value
   display.value = squareNumberOutput;
 
-    // And then we reset the display array
-  calculatorDisplayContents = [square_number_output];
+  // And then we reset the display array
+  calculatorDisplayContents = [squareNumberOutput];
 }
 
 // Removes last element of the calculatorDisplayContents and then re-renders the display value.
